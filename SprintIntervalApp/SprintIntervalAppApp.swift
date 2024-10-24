8de00b11f7ba516f8ddc5daaea498312485334fc
Lucas Romero Magaña <lucas.romero@vasscompany.es>
2024-10-24 10:43:17 +0200

    Initial Commit

---
+++ SprintIntervalApp/SprintIntervalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintIntervalAppApp.swift
+//  SprintIntervalApp
+//
+//  Created by Lucas Romero Magaña on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SprintIntervalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
